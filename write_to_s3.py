ac8778c42f529387186e719eb88200d7476d4a44
Armin Efendic <arminefendic12@gmail.com>
2022-10-23 18:25:46 -0600

    security clean up

--- write_to_s3.py
+++ write_to_s3.py
@@ -4,12 +4,14 @@ import os
 from botocore.exceptions import ClientError
 import logging
 import re
+from keys import aws_access_key_id
+from keys import aws_secret_access_key
 
 def write_to_s3(file_dir, bucket_name):
     file_match_counter = 0
     client = boto3.client('s3',
-                            aws_access_key_id='AKIAUUSGHFEUAWFBD6NR',
-                            aws_secret_access_key='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'
+                            aws_access_key_id= aws_access_key_id,
+                            aws_secret_access_key=aws_secret_access_key
     )
 
     for file in os.listdir(file_dir):
@@ -19,7 +21,7 @@ def write_to_s3(file_dir, bucket_name):
             s3_file_path = 'python_data/' + str(file)
             try:
                 client.upload_file(local_file_path, bucket, s3_file_path)
-                file_match_counter =+ 1
+                file_match_counter += 1
                 os.rename(local_file_path, local_file_path + "_OK")
                 print('uploaded file')
             except ClientError as e:
